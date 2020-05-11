19a50bc3a2cf2e02b76732b0b82917a6a028ecef
Andre Freeman <asf@mail.usf.edu>
2020-05-11 15:10:52 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-import styled from "styled-components";
-import { Route, Link } from "react-router-dom";
+import { Route} from "react-router-dom";
 import WelcomePage from "./components/welcomePage";
 import NavBar from "./components/navBar";
-import axios from "axios";
 import PortfolioList from "./components/portfolioList"
 
 
